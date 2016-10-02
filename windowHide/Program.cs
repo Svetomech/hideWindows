5f8d90a87fa06477d18cf7ea6973bd95dad4a897
Andrey Belyaev <svetomech@gmail.com>
2016-10-02 20:06:19 +0300

    Removed unused main form

--- windowHide/Program.cs
+++ windowHide/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace windowHide
@@ -12,11 +9,12 @@ namespace windowHide
     /// The main entry point for the application.
     /// </summary>
     [STAThread]
-    static void Main()
+    static void Main(string[] args)
     {
-      Application.EnableVisualStyles();
-      Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run(new Form1());
+      foreach (string arg in args)
+      {
+        // Svetomech.Utilities
+      }
     }
   }
 }
