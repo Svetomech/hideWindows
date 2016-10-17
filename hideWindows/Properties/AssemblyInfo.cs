19ebeffb953093b8a213346add360aa8a070fbf8
Andrey Belyaev <svetomech@gmail.com>
2016-10-18 01:42:13 +0300

    Fixed small grammar mistake

--- hideWindows/Properties/AssemblyInfo.cs
+++ hideWindows/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("hideWindows")]
-[assembly: AssemblyDescription("CLI to hide/show the windows of a specified processes")]
+[assembly: AssemblyDescription("CLI to hide/show windows of a specified processes")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
